c123d6602eb0b2479223b385600814033c0eb68d
IamMachir <marveltechofficial@gmail.com>
2026-05-27 21:52:11 +0300

    Add packages/ui-components/src/index.ts

---
+++ packages/ui-components/src/index.ts
@@ -0,0 +1,36 @@
+// Base components
+export { Button }       from './components/Button';
+export { Input }        from './components/Input';
+export { Textarea }     from './components/Textarea';
+export { Select }       from './components/Select';
+
+// Task components
+export { TaskCard }     from './components/TaskCard';
+export { TaskModal }    from './components/TaskModal';
+export { KanbanColumn } from './components/KanbanColumn';
+
+// Badge components
+export { PriorityBadge } from './components/PriorityBadge';
+export { StatusBadge }   from './components/StatusBadge';
+
+// Progress
+export { ProgressBar }    from './components/ProgressBar';
+export { ProgressCircle } from './components/ProgressCircle';
+
+// Layout / Navigation
+export { Sidebar }   from './components/Sidebar';
+export { FilterBar } from './components/FilterBar';
+
+// User
+export { UserAvatar } from './components/UserAvatar';
+
+// Search
+export { SearchInput } from './components/SearchInput';
+
+// Stats & Info
+export { StatsCard }          from './components/StatsCard';
+export { EmptyState }         from './components/EmptyState';
+export { ConfirmationDialog } from './components/ConfirmationDialog';
+
+// Lib
+export { cn } from './lib/utils';
