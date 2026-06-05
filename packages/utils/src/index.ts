10b30e0e33abd8509cbba5f90d907fec7fbc6b2b
IamMachir <marveltechofficial@gmail.com>
2026-06-05 12:04:38 +0300

    Add new utilities to exports

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,20 +1,12 @@
-// Date utilities
-export * from './date';
+export { date, formatDate, isToday, isTomorrow, getDayName } from './date';
+export { capitalize, truncate, slugify } from './string';
+export { createTask, updateTask, deleteTask, filterTasks, getTotalProgress } from './task';
+export { filterByStatus, filterByPriority, filterByDate } from './filter';
+export { getFromStorage, saveToStorage, clearStorage } from './storage';
+export { generateId } from './id';
+export { validateEmail, validateTask } from './validation';
 
-// String utilities
-export * from './string';
-
-// Task utilities (types + priority/status helpers)
-export * from './task';
-
-// Filter & sort
-export * from './filter';
-
-// localStorage helpers
-export * from './storage';
-
-// ID generation
-export * from './id';
-
-// Validation
-export * from './validation';
+// NEW: Dark mode, theme, and bulk operations
+export { isDarkMode, lightColors, darkColors, priorities } from './theme';
+export { useDarkMode } from './useDarkMode';
+export { bulkDelete, bulkComplete, bulkChangePriority } from './bulkOperations';
